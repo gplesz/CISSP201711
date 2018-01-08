9454b4f450cefc4b624d67c004ac35ca9e2934af
gplesz <gabor.plesz@gmail.com>
2018-01-08 14:24:57 +0100

    #4: Input validation

--- InputValidationTest/InputValidationTest/Models/AccountViewModels/RegisterViewModel.cs
+++ InputValidationTest/InputValidationTest/Models/AccountViewModels/RegisterViewModel.cs
@@ -8,19 +8,22 @@ namespace InputValidationTest.Models.AccountViewModels
 {
     public class RegisterViewModel
     {
-        [Required]
-        [EmailAddress]
+        [Required] //Data range: kötelező megadni
+        [EmailAddress] //Data formatting: csak ami email címet jelent
         [Display(Name = "Email")]
         public string Email { get; set; }
 
-        [Required]
+        [Required] //data range: kötelező megadni
+        //data range: 0-100 karakter hosszú lehet
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        //Data type/data formatting: csillagokkal jelzi az adatbevitelt
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
         [Display(Name = "Confirm password")]
+        //meg kell egyeznie a Password mező tartalmával
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
     }

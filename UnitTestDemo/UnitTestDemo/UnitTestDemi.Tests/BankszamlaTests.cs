f3735f02fb6f2b51041935b3a2428a74c7a08a7a
gplesz <gabor.plesz@gmail.com>
2018-01-08 11:55:25 +0100

    #4: unit teszt példa

---
+++ UnitTestDemo/UnitTestDemo/UnitTestDemi.Tests/BankszamlaTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTestDemo;
+
+namespace UnitTestDemi.Tests
+{
+    [TestClass]
+    public class BankszamlaTests
+    {
+
+        /// <summary>
+        /// Ellenőrzi ezt:
+        /// egy új bankszámlán ha jóváírunk 5000 forintot,
+        /// akkor a bankszámla egyenlege 5000 ft kell, hogy legyen
+        /// </summary>
+        [TestMethod]
+        public void BankszamlaJovairasNoveliAzEgyenleget()
+        {
+            //Arrange: előkészítés, létrehozás
+            var bnkszmla = new Bankszamla("Teszt bankszámla");
+
+            //Act: művelet
+            bnkszmla.Jovairas(5000);
+
+            //Assert: ellenőrzés
+            Assert.AreEqual(4000, bnkszmla.Egyenleg);
+
+        }
+    }
+}
---
+++ UnitTestDemo/UnitTestDemo/UnitTestDemo/Bankszamla.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestDemo
+{
+    public class Bankszamla
+    {
+        private string nev;
+
+        public Bankszamla(string nev)
+        {
+            this.nev = nev;
+        }
+
+        public string Nev { get { return this.nev; } }
+
+        private int egyenleg;
+        public int Egyenleg { get { return this.egyenleg; } }
+
+        public void Terheles(int osszeg)
+        {
+            if (osszeg < 0)
+            {
+                throw new Exception("terhelni csak pozitív számot lehet");
+            }
+
+            if (Egyenleg < osszeg)
+            {
+                throw new Exception($"a terhelendő összeg {osszeg} nagyobb, mint a rendelkezésre álló összeg {egyenleg}");
+            }
+
+            egyenleg = egyenleg - osszeg;
+            System.Console.WriteLine($"{osszeg} terhelése megtörtént, új egyenleg: {egyenleg}");
+        }
+
+        public void Jovairas(int osszeg)
+        {
+            if (osszeg < 0)
+            {
+                throw new Exception("jovairni csak pozitív számot lehet");
+            }
+
+            egyenleg = egyenleg + osszeg;
+            System.Console.WriteLine($"{osszeg} jóváírása megtörtént, új egyenleg: {egyenleg}");
+        }
+
+
+
+    }
+}
